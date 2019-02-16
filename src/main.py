9e9af48c2b527c5f32536d61fa2b694c3b5e3916
emma <evareilles@kpler.com>
2019-02-16 17:53:34 +0100

    improve couple model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import os
+from datetime import datetime
 
+from src.domain.model_couple import exploration, satisfaction
 from src.services.csv_reader import CsvReader
 from src.services.map import Map
 
@@ -13,11 +15,20 @@ def main():
 
     hotels, employees = csv_reader.parse(HOTELS_DATA_FILE, 'hotel'), csv_reader.parse(EMPLOYEES_DATA_FILE, 'people')
 
-    _enrich_entity_with_point(map, hotels)
+    # _enrich_entity_with_point(map, hotels)
     _enrich_entity_with_point(map, employees)
+
+    employees = _enrich_employees_with_availabilities(employees)
 
     # 1) Call model couple
 
+    persons, disponibility_per_persons = [p['name'] for p in employees], {p['name']: p['availabilities'] for p in employees}
+
+    print('---- Exploration ----')
+    exploration_status, exploration_assignments, maximisation = exploration(persons, disponibility_per_persons)
+
+    print('---- Satisfaction ----')
+    satisfaction_status, satisfaction_assignments = satisfaction(persons, disponibility_per_persons, maximisation)
     # 2) Select a date to focus on / filter model_couples
 
     # 3) Call solver
@@ -26,13 +37,44 @@ def main():
 
 
 # /!\ Careful: impure function
-def _enrich_entity_with_point(map, entity):
-    entity_address_and_postcode = {
-        'address': entity.get('address'),
-        'postcode': entity.get('postcode'),
-    }
-    point = map.point(entity_address_and_postcode)
-    entity['point'] = point
+def _enrich_entity_with_point(map, entities):
+    for entity in entities:
+        entity_address_and_postcode = {
+            'address': entity.get('address'),
+            'postcode': entity.get('postcode'),
+        }
+        point = map.point(entity_address_and_postcode)
+        entity['point'] = point
+
+
+def _enrich_employees_with_availabilities(employees):
+    grouped_employees = {}
+    for employee in employees:
+        name_and_surname = '{} {}'.format(employee['name'].strip(), employee['surname'].strip())
+        new_availability = process_employee_availability(employee)
+        if name_and_surname not in grouped_employees:
+            grouped_employees[name_and_surname] = employee
+            grouped_employees[name_and_surname]['availabilities'] = new_availability
+        else:
+            new_availability = process_employee_availability(employee)
+            grouped_employees[name_and_surname]['availabilities'] += new_availability
+
+    return grouped_employees.values()
+
+
+def process_employee_availability(employee):
+    availability = datetime.strptime(employee['availability'], '%d/%m/%Y')
+    time_of_day = employee['time_of_day']
+    if time_of_day.strip().lower() == 'jour':
+        morning, afternoon = '{}{}{}{}'.format(availability.year, availability.month, availability.day, 0), \
+                             '{}{}{}{}'.format(availability.year, availability.month, availability.day, 1)
+        return [int(morning), int(afternoon)]
+    elif time_of_day.strip().lower() == 'matin':
+        morning = '{}{}{}{}'.format(availability.year, availability.month, availability.day, 0)
+        return [int(morning)]
+    else:
+        afternoon = '{}{}{}{}'.format(availability.year, availability.month, availability.day, 0)
+        return [int(afternoon)]
 
 
 if __name__ == '__main__':
