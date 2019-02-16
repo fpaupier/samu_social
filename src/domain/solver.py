13cf46dd88318b80514be6ba50f17ed7c6d4d2f7
François Paupier <francois.paupier@gmail.com>
2019-02-16 11:54:11 +0100

    WIP distances matrix construction

--- src/domain/solver.py
+++ src/domain/solver.py
@@ -1,9 +1,39 @@
-from ortools.constraint_solver import pywrapcp
-from ortools.constraint_solver import routing_enums_pb2
-
 """
 Determine an optimal list of hotel to visit.
 """
+
+from ortools.constraint_solver import pywrapcp
+from ortools.constraint_solver import routing_enums_pb2
+
+from src.services.map import Map
+
+
+def get_distances_matrix(addresses):
+    """Compute the distance matrix (distance between each hotels).
+    Returns a triangular matrix.
+
+    Args: 
+        addresses(list[dict]): list of address, each dict has the struct
+            {'address': 'Avenue Winston Churchill', 'postcode': 27000}
+    Returns:
+        distances(list[list[int]]): matrix of distances
+
+    """
+    map = Map()
+    distances = []
+
+    for address1 in addresses:
+        src_address = {'address': address1.get('address'), 'postcode': address1.get('postcode')}
+        src_dist = [] 
+        for address2 in addresses:
+            target_address = {'address': address2.get('address'), 'postcode': address2.get('postcode')}
+            point1 = map.point(src_address)
+            point2 = map.point(target_address)
+
+            distance = map.distance(point1, point2)
+            src_dist.append(distance)
+        distances.append(src_dist)
+    return distances
 
 ###########################
 # Problem Data Definition #
@@ -12,26 +42,7 @@ def create_data_model():
   """Creates the data for the example."""
   data = {}
   # Array of distances between locations.
-  _distances = \
-          [
-           [0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662],
-           [548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210],
-           [776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754],
-           [696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358],
-           [582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244],
-           [274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708],
-           [502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480],
-           [194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856],
-           [308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514],
-           [194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468],
-           [536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354],
-           [502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844],
-           [388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730],
-           [354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536],
-           [468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194],
-           [776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798],
-           [662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0]
-          ]
+  _distances = get_distances_matrix(addresses)
   data["distances"] = _distances
   data["num_locations"] = len(_distances)
   data["num_vehicles"] = 4
