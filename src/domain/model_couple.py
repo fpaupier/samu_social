b51fec53b8b76dd54611e823b728eea5baeacb9b
emma <evareilles@kpler.com>
2019-02-16 17:53:34 +0100

    improve couple model

--- src/domain/model_couple.py
+++ src/domain/model_couple.py
@@ -1,3 +1,5 @@
+import os
+
 from ortools.sat.python import cp_model
 
 from src.domain.availability_model import read_employees_file
@@ -6,88 +8,111 @@ from src.domain.utils import SolverStatus
 RESULTS_COUNT_LIMIT = 10
 
 
-def create_model(persons, dispos_per_person):
+def create_couples(persons):
+    """
+    Create a list of couples without duplicate
+
+    Args:
+        persons (list[str]):
+
+    Returns:
+        list[set(str)]
+    """
+    list_of_couples = []
+    for p1 in persons:
+        for p2 in persons:
+            couple = {p1, p2}
+            if couple not in list_of_couples:
+                list_of_couples += [couple]
+    return list_of_couples
+
+
+def create_model(persons, list_of_couples, dispos_per_person):
+    """
+    Build the model
+    """
 
     model = cp_model.CpModel()
 
+    print(list_of_couples)
     couples = {}
     dispos_per_couples = {}
-    for p1 in persons:
-        couples[p1] = {}
-        dispos_per_couples[p1] = {}
-        for p2 in persons:
-            couples[p1][p2] = model.NewBoolVar('{}_coupled_with_{}'.format(p1, p2))
+    for i, couple in enumerate(list_of_couples):
+        if len(couple) == 2:
+            p1, p2 = couple
+            couples[i] = model.NewBoolVar('{}_coupled_with_{}'.format(p1, p2))
             dispos_p1 = dispos_per_person[p1]
             dispos_p2 = dispos_per_person[p2]
-            dispos_per_couples[p1][p2] = [d_p1 for d_p1 in dispos_p1 for d_p2 in dispos_p2 if d_p1 == d_p2]
+            dispos_per_couples[i] = [d_p1 for d_p1 in dispos_p1 for d_p2 in dispos_p2 if d_p1 == d_p2]
 
-            # A person cannot be coupled with itself
-            if p1 == p2:
-                dispos_per_couples[p1][p2] = []
-                model.Add(couples[p1][p2] == False)
+        # A person cannot be coupled with itself
+        if len(couple) == 1:
+            couples[i] = model.NewBoolVar('{}_coupled_alone'.format(couple))
+            dispos_per_couples[i] = []
+            model.Add(couples[i] == False)
 
     # Define couples: 1 person linked to one other exactly
     for p1 in persons:
-        model.Add(sum(couples[p1][p2] for p2 in persons) == 1)
-
-    # Define couples: person 1 linked to person 2 implies that person 2 is linked to person 1
-    for p1 in persons:
-        for p2 in persons:
-            model.Add(couples[p1][p2] == couples[p2][p1])
+        model.Add(sum(couples[i] for i, couple in enumerate(list_of_couples) if p1 in couple) <= 1)
 
     return model, couples, dispos_per_couples, dispos_per_couples
 
 
 def exploration(persons, dispos_per_persons):
-    model, couples, dispos_per_persons, dispos_per_couples = create_model(persons, dispos_per_persons)
 
-    model.Maximize(sum(len(dispos_per_couples[p1][p2])*couples[p1][p2] for p1 in persons for p2 in persons))
+    list_of_couples = create_couples(persons)
+    model, couples, dispos_per_persons, dispos_per_couples = create_model(persons, list_of_couples, dispos_per_persons)
+
+    model.Maximize(sum(len(dispos_per_couples[i])*couples[i] for i, couple in enumerate(list_of_couples)))
+    # model.Maximize(sum(len(dispos_per_couples[tuple({p1,p2})])*couples[tuple({p1,p2})] for p1 in persons for p2 in persons))
+
 
     solver = cp_model.CpSolver()
     status = solver.Solve(model)
     status = solver.StatusName(status)
 
     if SolverStatus.success(status):
-        return status, print_solutions(solver, persons, couples, dispos_per_couples), solver.ObjectiveValue()
+        return status, print_solutions(solver, list_of_couples, couples, dispos_per_couples), solver.ObjectiveValue()
 
     else:
         print('Cannot find couples :\'(')
-        return status, 0, {}
+        return status, {}, 0
 
 
 class VarArrayAndObjectiveSolutionPrinter(cp_model.CpSolverSolutionCallback):
     """Print and save solutions."""
 
-    def __init__(self, variables, persons, dispos_per_couples):
+    def __init__(self, variables, list_of_couples, dispos_per_couples):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__variables = variables
         self.__solution_count = 0
         self.__solution_limit = int(RESULTS_COUNT_LIMIT)
         self.solutions = []
-        self.persons = persons
+        self.list_of_couples = list_of_couples
         self.dispos_per_couples = dispos_per_couples
 
     def save_solutions(self, solution):
         print('Solution {}'.format(self.__solution_count))
-        assignments = print_solutions(self, self.persons, solution, self.dispos_per_couples)
+        assignments = print_solutions(self, self.list_of_couples, solution, self.dispos_per_couples)
         self.solutions.append(assignments)
 
     def NewSolution(self):
         self.__solution_count += 1
         self.save_solutions(self.__variables)
-        if self.__solution_count >= self.__solution_limit:
-            self.StopSearch()
+        # if self.__solution_count >= self.__solution_limit:
+        #     self.StopSearch()
 
     def solution_count(self):
         return self.__solution_count
 
 
 def satisfaction(persons, dispos_per_persons, maximisation):
-    model, couples, dispos_per_persons, dispos_per_couples = create_model(persons, dispos_per_persons)
+    list_of_couples = create_couples(persons)
+    model, couples, dispos_per_persons, dispos_per_couples = create_model(persons, list_of_couples, dispos_per_persons)
 
-    model.Add(sum(len(dispos_per_couples[p1][p2])*couples[p1][p2] for p1 in persons for p2 in persons) == int(maximisation))
+    model.Add(sum(len(dispos_per_couples[i])*couples[i] for i, couple in enumerate(list_of_couples)) == int(maximisation))
 
-    solution_printer = VarArrayAndObjectiveSolutionPrinter(couples, persons, dispos_per_couples)
+    solution_printer = VarArrayAndObjectiveSolutionPrinter(couples, list_of_couples, dispos_per_couples)
     solver = cp_model.CpSolver()
 
     status = solver.SearchForAllSolutions(model, solution_printer)
@@ -99,28 +124,33 @@ def satisfaction(persons, dispos_per_persons, maximisation):
     return status, assignements
 
 
-def print_solutions(solver, persons, couples, dispos_per_couples):
+def print_solutions(solver, list_of_couples, couples, dispos_per_couples):
     assigments = {}
-    for p1 in persons:
-        assigned_p2 = [p2 for p2 in persons if solver.Value(couples[p1][p2])][0]
-        if set((p1,assigned_p2)) not in [set(k) for k in assigments.keys()]:
-            assigments[(p1,assigned_p2)] = dispos_per_couples[p1][assigned_p2]
-    for (p1, p2), dispos in assigments.items():
-        print('{} assigned to {} with dispo {}'.format(p1,p2, dispos_per_couples[p1][p2]))
+    assigned_couples = [(i, couple) for i, couple in enumerate(list_of_couples) if solver.Value(couples[i])]
+
+    for i, couple in assigned_couples:
+        assigments[tuple(couple)] = dispos_per_couples[i]
+        p1, p2 = couple
+        print('{} assigned to {} with dispo {}'.format(p1, p2, dispos_per_couples[i]))
     return assigments
 
 
 def main():
-    # import ipdb; ipdb.set_trace()
-    data = read_employees_file('/Users/emericbris/Downloads/55/fichier-salarie.csv')
+    data_directory = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'fichier-salarie.csv')
+    data = read_employees_file(data_directory)
     persons = [x['name'] for x in data]
     dispos_per_persons = {x['name']: x['availabilities'] for x in data}
 
-    persons = ['Em', 'Pop', 'E', 'Palpal']
-    dispos_per_persons = {'Em': [[1,4],[4,8],[12, 16]],
-                          'Pop': [[1,4],[12,16],[16, 20]],
-                          'E': [[4,8],[12,16],[16, 20]],
-                          'Palpal': [[12,16],[16, 20]]}
+    # persons = ['Em',
+    #            'Pop',
+    #            'E',
+    #            'Palpal',
+    #            ]
+    # dispos_per_persons = {'Em': [[1,4],[4,8],[12, 16]],
+    #                       'Pop': [[1,4],[12,16],[16, 20]],
+    #                       'E': [[4,8],[12,16],[16, 20]],
+    #                       'Palpal': [[12,16],[16, 20]],
+    #                       }
 
     print('---- Exploration ----')
     exploration_status, exploration_assignments, maximisation = exploration(persons, dispos_per_persons)
