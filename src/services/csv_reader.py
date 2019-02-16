61823f1104031db6b52ae857c63bbb38bb0b4722
François Paupier <francois.paupier@gmail.com>
2019-02-16 09:09:33 +0100

    format source

--- src/services/csv_reader.py
+++ src/services/csv_reader.py
@@ -16,10 +16,11 @@
 import csv
 import os
 import argparse
-import json 
+import json
 
 
 dirname = os.path.dirname(__file__)
+
 
 def parse_csv(source, csv_type):
     """
@@ -30,42 +31,40 @@ def parse_csv(source, csv_type):
     Return:
         json_path: path to the file containing the adress as json format. 
     """
-    f_name = source.split('.')[0]
+    f_name = source.split(".")[0]
     json_path = "{0}-{1}.json".format(f_name, csv_type)
 
     adresses = []
-    with open(source, "r", encoding='utf-8') as f:
+    with open(source, "r", encoding="utf-8") as f:
         reader = csv.reader(f, delimiter=";")
         for i, line in enumerate(reader):
             if csv_type == "people" and i > 0:
-                adresses.append("{} {} {} {} {}".format(line[2], line[3], line[4], line[5], line[6]))
+                adresses.append(
+                    "{} {} {} {} {}".format(line[2], line[3], line[4], line[5], line[6])
+                )
             if csv_type == "hotel" and i > 0:
-                if line[2] == '0': # Only consider non removed hotel
+                if line[2] == "0":  # Only consider non removed hotel
                     adresses.append("{} {} {}".format(line[7], line[8], line[9]))
 
-    with open(json_path, 'w', encoding='utf8') as outfile:
+    with open(json_path, "w", encoding="utf8") as outfile:
         json.dump(adresses, outfile, ensure_ascii=False)
 
     return json_path
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="A scraper to obtain addresses from a csv"
     )
-    parser.add_argument('-t',
-                        '--csv_type',
-                        help='type of csv file to parse: `hotel` or `people`',
-                        type=str,
-                        default='hotel',
-                        )
+    parser.add_argument(
+        "-t",
+        "--csv_type",
+        help="type of csv file to parse: `hotel` or `people`",
+        type=str,
+        default="hotel",
+    )
 
-    parser.add_argument('-s',
-                        '--source',
-                        help='path to the source csv file',
-                        type=str,
-                        )
+    parser.add_argument("-s", "--source", help="path to the source csv file", type=str)
 
     args = parser.parse_args()
     parse_csv(args.source, args.csv_type)
